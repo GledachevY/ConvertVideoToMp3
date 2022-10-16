eb4dc0e68cb48fa4b36e4e17e02d119b15963c09
Yavor Gledachev <yavor.gledachev@accedia.com>
2022-10-16 13:52:17 +0300

    map song names

--- converter.js
+++ converter.js
@@ -1,34 +1,47 @@
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
 
-const main = () => {
+
+const main = async () => {
+    const videosInfo = await getVideosInfo()
+
     ffmpeg.setFfmpegPath('C:/Users/yavor.gledachev/Downloads/ffmpeg-5.1.2-essentials_build/ffmpeg-5.1.2-essentials_build/bin/ffmpeg.exe')
     const fileNames = fs.readdirSync('./playtube');
     const videoFileNames = fileNames.filter(f => f.includes('mp4'))
-    const videos = readVideos(videoFileNames)
+    convertVideos(videoFileNames, videosInfo)
 }
 
-const readVideos = (fileNames) => {
-    const videos = [];
-
+const convertVideos = (fileNames, videosInfo) => {
     fileNames.forEach(f => {
-        const video = fs.readFileSync(`./playtube/${f}`);
         try {
-            ffmpeg(`./playtube/${f}`).toFormat('mp3').on('end', function () {
-                console.log('here')
-
-            }).on('error', function (error) {
-                console.log(error)
-            }).save(`./songs/${f}.mp3`);//path where you want to save your file
-            //audioFiles.push(audioFile)
+            let title = videosInfo.find(info => f.includes(info.id))?.title
+            if (title?.includes('/')) title = title.replaceAll('/', '-')
+            if (title?.includes('|')) title = title.replaceAll('|', '-')
+            if (title?.includes(':')) title = title.replaceAll(':', '-')
+            if (title?.includes('"')) title = title.replaceAll('"', '-')
+            ffmpeg(`./playtube/${f}`).audioQuality(0).toFormat('mp3')
+                .on('end', function () {
+                    console.log('here')
+                }).on('error', function (error) {
+                    console.log(error)
+                })
+                .save(`./songs/${title ? title : f}.mp3`);//path where you want to save your file
         } catch (error) {
             console.log(error)
         }
     })
+}
 
-    return videos
+const getVideosInfo = async () => {
+    const db = await open({
+        filename: 'playTube.db',
+        driver: sqlite3.Database
+    })
+
+    const videosInfo = await db.all('SELECT * FROM videos')
+    return videosInfo
 }
 
 main()
-
-module.exports.converter = main
